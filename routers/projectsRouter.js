1bbdd925ef1847686642616acef2dfa0d69a5bd1
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-10-09 14:09:22 -0700

    Getting the post figured out

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -6,6 +6,16 @@ router.get('/projects/:id', validateProjectId(), (req, res) => {
     return res.status(200).json(req.project)
 })
 
+router.post('/projects', validatePost(), (req, res, next) => {
+    projects.insert(req.body)
+    .then(project => {
+        return res.status(201).json(project)
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
 // Custom Middleware
 function validateProjectId() {
     return (req, res, next) => {
@@ -23,6 +33,26 @@ function validateProjectId() {
     .catch(error => {
         next(error)
     })
+    }
+}
+
+function validatePost() {
+    return (req, res, next) => {
+        if(Object.keys(req.body).length === 0) {
+            return res.status(400).json({
+                errorMessage: "missing project data"
+            })
+        } else if(!req.body.name) {
+            return res.status(400).json({
+                errorMessage: "Missing name field"
+            })
+        } else if(!req.body.description) {
+            return res.status(400).json({
+                errorMessage: "Missing description field"
+            })
+        } else {
+            next()
+        }
     }
 }
 
